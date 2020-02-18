4f39564c2f1acc78f81eb4e04e16996259f8e260
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2020-02-18 23:50:32 +0000

    Added MenuManagement

---
+++ Assets/Menu/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Slider loadingSlider;
+
+    public void StartGame()
+    {
+        StartCoroutine(LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadScene(int sceneIndex)
+    {
+        var sceneLoadOperation = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+
+        while (!sceneLoadOperation.isDone)
+        {
+            loadingSlider.value = Mathf.Clamp01(sceneLoadOperation.progress / 0.9f);
+            yield return null;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
