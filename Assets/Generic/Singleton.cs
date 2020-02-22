50c31f835a0d96b601f18eeb9e7b830cf72ef686
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2020-02-22 22:00:03 +0000

    Singleton Added

---
+++ Assets/Generic/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T>
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance != null)
+                return instance;
+            else
+                Debug.LogError($"Instance of {typeof(T).Name} not set.");
+            return default;
+        }
+        set
+        {
+            if (instance == null)
+                instance = value;
+            else
+                Debug.LogError($"Instance of {typeof(T).Name} is already set.");
+        }
+    }
+
+}
