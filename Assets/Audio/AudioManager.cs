64bcb1ee14f38efe3e0443587476450fe9274a9d
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2020-02-25 18:34:47 +0000

    AudioManager Added

---
+++ Assets/Audio/AudioManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixerGroup audioMixerGroup;
+    public Sound[] allSounds;
+
+    private void Awake()
+    {
+        foreach (Sound sound in allSounds)
+        {
+            GameObject soundSource = new GameObject($"{sound.name} Source");
+            soundSource.transform.parent = this.transform;
+            sound.source = soundSource.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.outputAudioMixerGroup = audioMixerGroup;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+        }
+    }
+
+    public void Play(string soundName)
+    {
+        Sound currentSound = Array.Find(allSounds, sound => sound.name == soundName);
+        if (currentSound == null)
+            return;
+        currentSound.source.Play();
+    }
+    public void Stop(string soundName)
+    {
+        Sound currentSound = Array.Find(allSounds, sound => sound.name == soundName);
+        if (currentSound == null)
+            return;
+        currentSound.source.Stop();
+    }
+
+}
---
+++ Assets/Audio/Sound.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+    public int volume = 1;
+    public int pitch = 1;
+    public bool loop = false;
+    [HideInInspector] public AudioSource source;
+}
